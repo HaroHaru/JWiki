eb85f5af9d8af5e4936c87261400a24e13b48b86
haroharu <136150210+HaroHaru@users.noreply.github.com>
2026-05-09 14:35:22 +0900

    프레임 생성

--- src/app/App.java
+++ src/app/App.java
@@ -2,8 +2,19 @@ package src.app;
 
 import javax.swing.*;
 
+class MainFrame extends JFrame {
+    MainFrame() {
+        JFrame frame = new JFrame();
+
+        setTitle("JWiki");
+
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+    }
+}
+
 public class App {
     public static void main(String[] args) {
-        
+        MainFrame frame = new MainFrame();
     }
 }
